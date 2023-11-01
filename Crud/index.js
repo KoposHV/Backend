b58ad7f201fd9856fbe25c56f7d507f6ef6e72d2
Kopos <carlosh.viveros12@gmail.com>
2023-11-01 13:51:30 +0100

    backend

---
+++ Crud/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connect = require("./utils/db");
+
+const server = express();
+
+connect();
+
+const router = express.Router();
+
+server.use("/", router);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
